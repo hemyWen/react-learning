ca49608a6fa869047df38dff0fe012ceed92929c
whm <1135063561@qq.com>
2024-10-21 15:23:56 +0800

    错误边界

---
+++ src/pages/ErrorBoundary/index.tsx
@@ -0,0 +1,8 @@
+import ErrorBoundary from "@/components/ErrorBoundary";
+export default function ErrorBoundaryPage() {
+  return (
+    <ErrorBoundary>
+      <div>这是内容</div>
+    </ErrorBoundary>
+  );
+}
