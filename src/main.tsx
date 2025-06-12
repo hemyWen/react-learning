b5b7705d60a22cab180da706bfe7ae59ae8b85b2
whm <1135063561@qq.com>
2025-06-12 11:23:15 +0800

    branch1提交

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./App.css";
+console.log("Hello world!");
+const a = 1;
+const b = 2;
+const c = a * b;
+console.log(c);
+
 createRoot(document.getElementById("root")!).render(<App />);
