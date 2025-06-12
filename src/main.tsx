ef6693b42b8b97490b616d5803d421dff94f1ec6
whm <1135063561@qq.com>
2025-06-12 11:02:31 +0800

    fix: branch2修改

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
+console.log(Math.random());
 createRoot(document.getElementById("root")!).render(<App />);
